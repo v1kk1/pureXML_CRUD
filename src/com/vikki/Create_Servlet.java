4c6f8abaf1500df1cf2e920989490d71bf9f6aa0
v1kk1 <dgr8geek@gmail.com>
2011-10-15 16:38:53 +0530

    Revert "created the first jsp page that supports xhtml and xforms(it was a pretty tough work)"
    This reverts commit 17a6408e7f6a41f5d0ba07200047bc30442d4e07.

--- src/com/vikki/Create_Servlet.java
+++ src/com/vikki/Create_Servlet.java
@@ -1,8 +1,6 @@
 package com.vikki;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,10 +16,7 @@ public class Create_Servlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		PrintWriter writer=response.getWriter();
-		writer.print("hello world!!!");
-	
+		// TODO Auto-generated method stub
 	}
 
 }
