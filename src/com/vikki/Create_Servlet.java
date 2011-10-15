b35b87db86a74812f9d5a591ee4320bf5671453d
v1kk1 <dgr8geek@gmail.com>
2011-10-15 16:47:56 +0530

    jsp supports xhtml n xforms

--- src/com/vikki/Create_Servlet.java
+++ src/com/vikki/Create_Servlet.java
@@ -1,6 +1,8 @@
 package com.vikki;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +18,10 @@ public class Create_Servlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		PrintWriter writer=response.getWriter();
+		writer.print("hello!!");
+	
 	}
 
 }
